d278086a66a4648eff1321bf4de1a9a8981a2472
walid-saadelkhalk <walid.saad-el-khalid@laplateforme.io>
2024-01-03 09:31:43 +0100

    modifiaction print

--- jour01/job09.py
+++ jour01/job09.py
@@ -8,9 +8,7 @@ class Produit:
         return self.prixHT + self.prixHT * self.TVA / 100
 
     def afficher(self):
-        # print(f"Le produit {self.nom}:\n Prix TTC:{self.CalculerPrixTTC()}$\n Prix HT:{self.prixHT}$\n TVA:{self.TVA}%\n " "" )
-
-        return self.nom, self.prixHT, self.TVA, self.CalculerPrixTTC()
+        return f" le produit {self.nom} a un prix HT {self.prixHT}$. Pour une TVA de {self.TVA}% le prix coutera au final {self.CalculerPrixTTC()}$"
 
     def modificationNom(self):
         nom_modifie = input("Entrez le nouveau nom du produit: ")
@@ -22,15 +20,19 @@ class Produit:
 
 produit1 = Produit("Iphone", 1190, 20)
 produit1.afficher()
+print(produit1.afficher())
 
 produit2 = Produit("Sneakers", 1000, 20)
 produit2.afficher()
+print(produit2.afficher())
 
 produit3 = Produit("T-shirt", 100, 20)
 produit3.afficher()
+print(produit3.afficher())
 
 produit4 = Produit("Ordinateur", 750, 20)
 produit4.afficher()
+print(produit4.afficher())
 
 
 
